a76670938b0d578fb5456cb1f696cb1323e61299
Damian <damianprakash07@gmail.com>
2023-12-23 07:37:51 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,365 @@
+export const Bio = {
+  name: "Damian Prakash Murmu",
+  roles: ["Full Stack Developer", "UI/UX Designer", "Programmer"],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/DamianDev11",
+  resume: "https://drive.google.com/",
+  linkedin: "https://www.linkedin.com/in/",
+  twitter: "https://twitter.com/",
+  insta: "https://www.instagram.com/",
+  facebook: "https://www.facebook.com/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "React Router",
+        image:
+          "https://www.codesmith.io/hs-fs/hubfs/Blog%20Images/Blog%20Photos/react-router-logo.png?width=600&name=react-router-logo.png",
+      },
+
+      {
+        name: "Vite",
+        image: "https://vitejs.dev/logo-with-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://w7.pngwing.com/pngs/925/447/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo.png",
+      },
+
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Certifications",
+    skills: [
+      {
+        name: "GCP-Cloud Digital Leader",
+        image:
+          "https://i0.wp.com/shuvankar.com/wp-content/uploads/2022/09/GCP-ACE_Logo.png?fit=520%2C520&ssl=1",
+      },
+      {
+        name: "AWS Certified Cloud Practitioner",
+        image:
+          "https://images.credly.com/size/680x680/images/00634f82-b07f-4bbd-a6bb-53de397fc3a6/image.png",
+      },
+      {
+        name: "ITIL® 4 Foundation",
+        image:
+          "https://api.accredible.com/v1/frontend/credential_website_embed_image/badge/23336475",
+      },
+      {
+        name: "Cyber Security Audits",
+        image:
+          "https://api.accredible.com/v1/frontend/credential_website_embed_image/badge/27176890",
+      },
+      {
+        name: "Python for Data Science",
+        image:
+          "https://images.credly.com/size/680x680/images/84ac9eff-b8a2-4683-846b-f59887a73801/Python_101_Data_Science.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "company logo",
+    role: "Associate, Systems Administration",
+    company: "Kyndryl India Private Limited",
+    date: "September 2021 - Present",
+    desc: "•Responsible for installing, configuring, and maintaining middleware software components such as application servers, message queues, web servers, and integration platforms.Managed and maintained website security, including SSL certificate renewals to ensure secure connections for customers and clients.Ensuring the security of the middleware infrastructure and implementing security measures, such as access controls, encryption, and authentication mechanisms, to protect sensitive data and prevent unauthorized access.",
+    skills: [
+      "Ubuntu",
+      "Apache HTTP Server",
+      "Microsoft IIS",
+      "SSL cetificate"
+    ],
+    doc: "",
+  },
+
+  {
+    id: 1,
+    img: "company logo",
+    role: "Associate Technical Engineer/NOC Engineer",
+    company: "IBM India Private Limited",
+    date: "August 2020 - September 2021",
+    desc: "•Responsible for monitoring, controlling and operating systems, networks,applications and/or distributed systems.Conducting VMware vCenter health check and resolving L1 incidents related to VM’s, ESXi hosts and datastores.•Performing windows systems health testing and operational tasks.•Supporting multiple technical teams in 24x7 operational environments with high uptime requirements.",
+    skills: [
+      "VMware Vsphere",
+      "Windows System Admin",
+      "Virtualization"
+    ],
+    doc: "https://www.linkedin.com/posts/damianprakashmurmu_windows-server-2012-certification-activity-6743581316891099136-9biL?utm_source=share&utm_medium=member_desktop",
+  },
+  {
+    id: 2,
+    img: "company logo",
+    role: "Cyber Security Intern",
+    company: "Verzeo",
+    date: "June 2020 - August 2021",
+    desc: ["During the tenure, I came across various insights on application of secure email through PGP email system.Installing and sending message using PGP. The curriculum also included how to ensure privacy while sending mail and many other networking concepts."],
+    skills: [
+      "wireshark",
+      "Nessus Professional",
+      "TCP packets transmission",
+      "DNS transmission",
+      "TLS transmission",
+      "ARP transmission",
+      "HTTP transmission",
+      "Spoofing and Phishing",
+      "Gsuite Toolbox",
+      "PGP",
+    ],
+    doc: "https://media.licdn.com/dms/image/C4E22AQH3CgZ_eY6vBA/feedshare-shrink_2048_1536/0/1607607735830?e=1700697600&v=beta&t=675FL65AQxEEg5HtPDP0iDBtVUke4mAE-oEZstA8m3k",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStZ9TzE21iDyWxk4UQB0LneOkxINfA6SAhGHlQQn8EKBVbnnQTrvPE4fV0luGcDRxN34Q&usqp=CAU",
+    school: "JAIN UNIVERSITY - SCHOOL OF ENGINEERING AND TECHNOLOGY, Bangalore",
+    date: "Aug 2016 - Aug 2020",
+    grade: "7.483 CGPA",
+    desc: "",
+    degree: "BTech, Cloud Technology and Information Security",
+  },
+  {
+    id: 1,
+    img: "https://sjb.campussoft.in/Images/Logo2.jpg  ",
+    school: "ST. JOSEPH'S SCHOOL",
+    date: "April 2016",
+    grade: "",
+    desc: "I completed my class 12 high school education at Methodist School, Dankuni, where I studied Science with Computer Science.",
+    degree: "ISC(XII)",
+  },
+  {
+    id: 2,
+    img: "https://sjb.campussoft.in/Images/Logo2.jpg",
+    school: "ST. JOSEPH'S SCHOOL",
+    date: "April 2014",
+    grade: "",
+    desc: "I completed my class 10 education at Methodist School, Dankuni, where I studied Science with Computer Application.",
+    degree: "ICSE(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Blog-Chef",
+    date: "Oct 2023",
+    description:
+      "Blog Application",
+    image:
+      "https://media.licdn.com/dms/image/D562DAQFHa94NvMrHeA/profile-treasury-image-shrink_800_800/0/1698522145699?e=1699171200&v=beta&t=wD259VeUU6co4aIj-m8HAFbTPePuW7k4XPjSXDJIFUU",
+    tags: [
+      "Eslint",
+      "Prettier",
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+    ],
+    category: "web app",
+    github: "https://github.com/DamianDev11/blog-chef",
+    webapp: "https://blog-chef.onrender.com/",
+  },
+  {
+    id: 0,
+    title: "BlogMe",
+    date: "Oct 2023",
+    description:
+      "Full Stack Blog Application with enhanced features",
+    image:
+      "https://media.licdn.com/dms/image/D562DAQFRJHPCrm3LZg/profile-treasury-image-shrink_800_800/0/1698520044057?e=1699171200&v=beta&t=LLWxUhPrT0YWjjS3cyZSCt_H5XLKdlnIRm8MRBvsFlk",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "React-router-dom","AXIOS"],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Podstream",
+    webapp: "https://podstream.netlify.app/",
+    member: [
+      {
+        name: "Damian Prakash Murmu",
+        img: "https://media.licdn.com/dms/image/D5603AQGC8l6VtF-rKA/profile-displayphoto-shrink_800_800/0/1694771720999?e=1704326400&v=beta&t=B0veKbWwLkE5Uf1q9-iOaEg8kce9YfXoIqeIYNKnb8s",
+        linkedin: "https://www.linkedin.com/in/damianprakashmurmu/",
+        github: "https://github.com/DamianDev11",
+      }
+    ],
+  },
+  {
+    id: 1,
+    title: "BlogIt",
+    date: "Oct 2023",
+    description:
+      "Simple Blog app",
+    image:
+    "proj image",
+    tags: [
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+    ],
+    category: "web app",
+    github: "https://github.com/DamianDev11",
+    webapp: "https://vexa-app.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Project1",
+    date: "Jan 2023 - Mar 2023",
+    description:
+    "proj desc",
+    image:
+      "project image",
+    tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+    category: "machine learning",
+    github: "https://github.com/DamianDev11",
+    webapp: "https://brain-tumor.netlify.app/",
+    member: [
+      {
+        name: "Damian Prakash Murmu",
+        img: "Damian Prakash Murmu image",
+        linkedin: "Damian Prakash Murmu linkedIn URL",
+        github: "https://github.com/DamianDev11",
+      }
+    ],
+  },
+  {
+    id: 3,
+    title: "Project2",
+    date: "Dec 2021 - Apr 2022",
+    description:
+    "proj desc",
+    image:
+      "project image",
+    tags: ["Kotlin", "MVVM", "Room Database", "Google Drive Cloud API"],
+    category: "android app",
+    github: "https://github.com/DamianDev11",
+    webapp: "https://play.google.com/store/apps/details?id=com.rishav.buckoid",
+  },
+  {
+    id: 10,
+    title: "Project3",
+    date: "Jun 2023 - Jul 2023",
+    description:
+    "proj desc",
+    image:
+      "project image",
+    tags: ["React Native", "JavaScript", "Axios"],
+    category: "android app",
+    github: "https://github.com/DamianDev11",
+    webapp: "https://github.com/rishavchanda/Job-finder-App",
+  },
+  {
+    id: 4,
+    title: "Project4",
+    date: "Jul 2021",
+    description:
+    "proj desc",
+    image:
+      "project image",
+    tags: ["React Js", "Firebase", "Firestore", "Node JS"],
+    category: "web app",
+    github: "https://github.com/DamianDev11",
+    webapp: "https://whatsapp-clone-rishav.web.app",
+  },
+  {
+    id: 5,
+    title: "Project5",
+    date: "Jun 2021",
+    description:
+      "proj desc",
+    image:
+      "project image",
+    tags: ["React Js", "Local Storage", "AWS Auth", "Node JS"],
+    category: "web app",
+    github: "https://github.com/DamianDev11",
+    webapp: "https://rishav-react-todo.netlify.app/",
+  },
+  {
+    id: 6,
+    title: "Project6",
+    date: "Jun 2021",
+    description:
+      "A simple react app that shows the characters of the famous TV series Breaking Bad. It uses the Breaking Bad API to fetch the data. It also has a search bar to search for a particular character.",
+    image:
+      "project image",
+    tags: ["React Js", "API", "Axios", "Node JS"],
+    category: "web app",
+    github: "https://github.com/DamianDev11",
+    webapp: "https://breaking-bad-webapp.netlify.app",
+  }
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
